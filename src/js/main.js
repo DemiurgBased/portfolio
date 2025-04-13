1849799e8a163a8dbab8db5c9c665691cbca8118
Demiurg <danzopoi228@mail.ru>
2025-04-13 05:04:26 +0300

    added header, no adaptive

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+async function loadHTML(url, elementId) {
+    const response = await fetch(url);
+    const html = await response.text();
+    document.getElementById(elementId).innerHTML = html;
+}
+
+loadHTML('./html/components/header', 'header');
+// loadHTML('./html/sections/hero.html', 'hero');
