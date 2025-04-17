f8d67edafb435fc04a4847340ca00873c9e8a762
Demiurg <danzopoi228@mail.ru>
2025-04-17 13:19:10 +0300

    added trainers-section, adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,49 @@
-import { UseLoad } from './components/load.js';
+import Swiper from 'swiper';
+import { Navigation, Scrollbar, } from 'swiper/modules';
 import { UseBurger } from './components/burger.js';
-UseLoad();
+
 UseBurger();
 
+// Slider(trainers__slider)
+new Swiper('.trainers__slider', {
+  modules: [Navigation, Scrollbar],
+  navigation: {
+    prevEl: '.trainers__slider-prev',
+    nextEl: '.trainers__slider-next'
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    381: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    577: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    769: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    993: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1201: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1441: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  }
+  // loop: true,
+});
